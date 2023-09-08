ba20d2ac2774655570b6af91c64ebd2e303b0bd5
Di10n <dilonchugh@gmail.com>
2023-09-08 11:21:59 -0700

    Desktop-first breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@
 module.exports = {
   content: ["index.html"],
   theme: {
+    screens: {
+      '2xl': {'max': '1535px'},
+      'xl': {'max': '1279px'},
+      'lg': {'max': '1023px'},
+      'md': {'max': '767px'},
+      'sm': {'max': '639px'},
+    },
     extend: {
       animation: {
         bounce: 'bounce 1s infinite',
